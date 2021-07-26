6804e9bea1eedec890be9fc9ec7a33b32941064a
Nicolas Béreux <nicolas.bereux@gmail.com>
2021-07-26 10:47:03 +0200

    trainTMC1D5C 2nd phase lr 0.01

--- src/trainTMCfromFile.py
+++ src/trainTMCfromFile.py
@@ -12,7 +12,7 @@ torch.set_num_threads(4)
 data = np.genfromtxt('../dataset/data_5.dat')
 data = torch.tensor((data+1)/2, device=device, dtype=dtype)
 
-fmodel = h5py.File("../model/AllParametersTMCRBM_NGibbs_25_Nh20_Nv1000_Nmb200_Nepoch50_lr_0.1_N20000_Npoint1000_Nchain15.h5", "r")
+fmodel = h5py.File("../model/AllParametersTMCRBM_NGibbs_25_Nh100_Nv1000_Nmb200_Nepoch50_lr_0.1_N20000_Npoint1000_Nchain15.h5", "r")
 
 Nv = data.shape[1]
 Nh = 100
@@ -20,13 +20,13 @@ Nh = 100
 verbose = 0
 save_fig = True
 
-lr = 0.001
+lr = 0.01
 NGibbs = 50
 it_mean = 20
 
 mb_s = 200
 num_pcd = 200
-ep_max = 1000
+ep_max = 50
 N = 20000
 nb_chain = 15
 nb_point = 1000
